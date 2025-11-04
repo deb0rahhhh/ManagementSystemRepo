389cbbda0852587179832cd8673a826aca7c6f67
N1184291 <debby.adekore-otu2023@my.ntu.ac.uk>
2025-11-04 20:16:17 +0000

    created a function that asks the user what role they are playing, will handle input validation soon

--- ManagementSystem/ManagementSystem/ManagementSystem.cpp
+++ ManagementSystem/ManagementSystem/ManagementSystem.cpp
@@ -1,23 +1,28 @@
 // ManagementSystem.cpp
-
+// will be run in CLI environment
 
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-int main()
-{
-   cout << "Hello World!\n";
+// function to display menu opt based on employee or manager rolw
+// 
+
+void displayUserMenu() {
+	string user1;
+	cout << "**** Welcome to the Warehouse Inventory Management System ****" << endl;
+	cout << "Select role: ";
+
+	cin >> user1;
+	cout << "You selected: " << user1 << endl;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main()
+{
+	displayUserMenu();
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+   return 0;
+	   
+}
