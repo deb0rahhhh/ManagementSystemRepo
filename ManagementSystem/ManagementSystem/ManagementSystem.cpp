76424fc8ed1246da3df33849c4905cb3c8c7c111
N1184291 <debby.adekore-otu2023@my.ntu.ac.uk>
2025-11-04 14:41:26 +0000

    This is another test.

--- ManagementSystem/ManagementSystem/ManagementSystem.cpp
+++ ManagementSystem/ManagementSystem/ManagementSystem.cpp
@@ -1,12 +1,14 @@
-// ManagementSystem.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// ManagementSystem.cpp
+
 
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+   cout << "Hello World!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
